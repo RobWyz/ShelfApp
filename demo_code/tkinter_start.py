fb35c7e2c1bd4d69c70eb444251a249ec4fedc1b
Rob <rwyzgolik@gmail.com>
2018-11-25 23:46:51 +0100

    That would do for today, submit button finally doing it's job - that's good.

--- demo_code/tkinter_start.py
+++ demo_code/tkinter_start.py
@@ -1,9 +1,42 @@
 import tkinter as tk
 from tkinter import ttk
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="mydatabase"
+)
+
+mycursor = mydb.cursor()
 
 LARGE_FONT = ("Verdana", 11)
 NORM_FONT = ("Verdana", 10)
 SMALL_FONT = ("Verdana", 8)
+
+
+def create_submit(e1, e2, e3, e4, e5, e6)-> None:
+
+    user = {"Name": e1.get(), "Surname": e2.get(), "Nickname": e3.get(), "Password": e4.get(),
+            "Email": e5.get(), "Country": e6.get()}
+
+    values = (user["Name"], user["Surname"], user["Nickname"], user["Password"], user["Email"], user["Country"])
+
+    sql_insert = "INSERT INTO users (name, surname, nickname, password," \
+                 " email, country) VALUES (%s, %s, %s, %s, %s, %s)"
+
+    mycursor.execute(sql_insert, values)
+    mydb.commit()
+
+
+def delete_submit(e1, e2):
+
+    values = (e1.get(), e2.get())
+
+    mycursor.execute("DELETE FROM users WHERE nickname = %s \
+         AND password = %s", values)
+    mydb.commit()
 
 
 def popupmsg(msg):
@@ -80,24 +113,33 @@ class CreateAccountPage(tk.Frame):
         label_4 = tk.Label(self, text="Password")
         label_5 = tk.Label(self, text="Email")
         label_6 = tk.Label(self, text="Country")
+
         entry_1 = tk.Entry(self, cursor="arrow")
         entry_2 = tk.Entry(self, cursor="man")
         entry_3 = tk.Entry(self, cursor="spider")
         entry_4 = tk.Entry(self, cursor="target", show="*")
         entry_5 = tk.Entry(self, cursor="star")
         entry_6 = tk.Entry(self, cursor="heart")
+
         label_1.grid(row=0, sticky="e")
         label_2.grid(row=1, sticky="e")
         label_3.grid(row=2, sticky="e")
         label_4.grid(row=3, sticky="e")
         label_5.grid(row=4, sticky="e")
         label_6.grid(row=5, sticky="e")
+
         entry_1.grid(row=0, column=1)
         entry_2.grid(row=1, column=1)
         entry_3.grid(row=2, column=1)
         entry_4.grid(row=3, column=1)
         entry_5.grid(row=4, column=1)
         entry_6.grid(row=5, column=1)
+
+        submit_button = ttk.Button(self, text="Apply Changes",
+                                   command=lambda: create_submit(entry_1, entry_2, entry_3, entry_4, entry_5, entry_6))
+        submit_button.grid(row=6, column=1)
+        return_button = ttk.Button(self, text="Take me home", command= lambda: controller.show_frame(StartPage))
+        return_button.grid(row=7, sticky="e")
 
 
 class UpdateEmailPage(tk.Frame):
@@ -124,6 +166,12 @@ class UpdateEmailPage(tk.Frame):
         entry_3.grid(row=2, column=1)
         entry_4.grid(row=3, column=1)
 
+        submit_button = ttk.Button(self, text="Apply Changes", command=lambda: popupmsg("Haven't been finished yet"))
+        submit_button.grid(row=4, column=1)
+        return_button = ttk.Button(self, text="Take me home", command=lambda: controller.show_frame(StartPage))
+        return_button.grid(row=5, sticky="e")
+
+
 class DeleteAccountPage(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -137,6 +185,11 @@ class DeleteAccountPage(tk.Frame):
         label_2.grid(row=1, sticky="e")
         entry_1.grid(row=0, column=1)
         entry_2.grid(row=1, column=1)
+
+        submit_button = ttk.Button(self, text="Apply Changes", command=lambda: delete_submit(entry_1, entry_2))
+        submit_button.grid(row=2, column=1)
+        return_button = ttk.Button(self, text="Take me home", command=lambda: controller.show_frame(StartPage))
+        return_button.grid(row=3, sticky="e")
 
 
 class ChangePasswordPage(tk.Frame):
@@ -163,6 +216,11 @@ class ChangePasswordPage(tk.Frame):
         entry_3.grid(row=2, column=1)
         entry_4.grid(row=3, column=1)
 
+        submit_button = ttk.Button(self, text="Apply Changes", command=lambda: popupmsg("Haven't been finished yet"))
+        submit_button.grid(row=4, column=1)
+        return_button = ttk.Button(self, text="Take me home", command=lambda: controller.show_frame(StartPage))
+        return_button.grid(row=5, sticky="e")
+
 
 class LogInPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -180,20 +238,11 @@ class LogInPage(tk.Frame):
         entry_1.grid(row=0, column=1)
         entry_2.grid(row=1, column=1)
 
+        submit_button = ttk.Button(self, text="Apply Changes", command=lambda: popupmsg("Haven't been finished yet"))
+        submit_button.grid(row=2, column=1)
+        return_button = ttk.Button(self, text="Take me home", command=lambda: controller.show_frame(StartPage))
+        return_button.grid(row=3, sticky="e")
+
 
 app = ShelfApp()
 app.mainloop()
-
-"""
-button1 = tkk.Button(self, text="Create an account", fg="purple",
-                         command=lambda: controller.show_frame(CreateAccountPage))
-        button2 = tkk.Button(self, text="Update your email", fg="blue",
-                         command=lambda: controller.show_frame(UpdateEmailPage))
-        button3 = tkk.Button(self, text="Change your password", fg="green",
-                         command=lambda: controller.show_frame(ChangePasswordPage))
-        button4 = tkk.Button(self, text="Log in", fg="orange",
-                         command=lambda: controller.show_frame(LogInPage))
-        button5 = tkk.Button(self, text="Delete an account", fg="red",
-                         command=lambda: controller.show_frame(DeleteAccountPage))
-
-"""
